c3cd80cbd851a6e73e39e10d35b05012c31f0321
root <root@188.225.75.34>
2021-05-06 06:51:24 +0300

    увеличение таймаута

--- app/Invoker.php
+++ app/Invoker.php
@@ -5,9 +5,21 @@ namespace RG;
 
 
 use GuzzleHttp\Client;
+use RG\Log;
 
 class Invoker
 {
+    private $log;
+
+    private $client;
+
+    public function __construct()
+    {
+        $this->log = new Log();
+
+        $this->client = new Client();
+    }
+
     public function invoke(Task $task)
     {
         $url = $task->url;
@@ -17,13 +29,18 @@ class Invoker
                 $task->login,
                 $task->password
             ],
-            'form_params' => json_decode($task->params, true)
+            'form_params' => json_decode($task->params, true),
+            'timeout' => 300.0,
+            'connect_timeout' => 300.0,
         ];
 
-        $client = new Client();
+        try {
+            $response = $this->client->post($url, $query);
+            return $response->getBody()->getContents();
 
-        $response = $client->post($url, $query);
-
-        return $response->getBody()->getContents();
+        } catch (\Exception $e) {
+            $this->log->fail($task);
+            die();
+        }
     }
 }
--- app/Log.php
+++ app/Log.php
@@ -15,4 +15,11 @@ class Log
 
         file_put_contents($_ENV['LOG'], "$time: для $task->url синхронизация сервиса c параметрами $task->params\n", FILE_APPEND);
     }
+
+    public function fail(Task $task)
+    {
+        $time = date('d.m.y H:i:s');
+
+        file_put_contents($_ENV['LOG'], "$time: <ОШИБКА> для $task->url c параметрами $task->params\n", FILE_APPEND);
+    }
 }
